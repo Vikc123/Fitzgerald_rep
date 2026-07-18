6340edd7a706817785b806ef06479756b1b9d115
admin <yoj30900@gmail.com>
2026-07-18 11:55:08 +1000

    22

--- knowledge_recovery/classes/linked_list.py
+++ knowledge_recovery/classes/linked_list.py
@@ -1,32 +1,45 @@
-class LinkedList:
+from Sem_3.Lab_1.main import Node
+
+
+class LinList():
     def __init__(self):
         self.head = None
         self.tail = None
-    def append(self, item):
+    def append(self, data):
         if self.head is None:
-            self.head = Node(item)
+            self.head = Node(data)
             self.tail = self.head
         else:
-            current = self.tail
-            self.tail.next = Node(item)
-            self.tail = self.tail.next
-            self.tail.prev = current
+            current = self.head
+            newNode = Node(data)
 
-class Node:
+            while(current.data > data and current.next is not None):
+                current = current.next
+
+            if self.head.data <= data:
+                newNode.next = current
+                current.prev = newNode
+                newNode.next = current
+                self.head = newNode
+
+            newNode.prev = current
+            current.next = newNode
+            self.tail = newNode
+
+
+class Node():
     def __init__(self, data):
         self.data = data
         self.next = None
         self.prev = None
 
 
-
-
 def main():
-    List = LinkedList()
-    collection = [1,2,3,4,5]
-    for item in collection:
-        List.append(item)
-    print(List)
+    List = LinList()
+    a = [10, 3, 7]
+    for i in a:
+        List.append(i)
+    print("done")
 
 if __name__ == "__main__":
     main()
