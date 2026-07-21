7e0b5f6e4ad95a5572cddc04b0ec1e6925793120
admin <yoj30900@gmail.com>
2026-07-21 11:37:26 +1000

    25

--- knowledge_recovery/Structures/BST/main.py
+++ knowledge_recovery/Structures/BST/main.py
@@ -1,7 +1,36 @@
 from mods import generator as gt
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, root, current):
+        if root is None:
+            self.root = current
+            return
+        else:
+            if current.data < root.data:
+                root.left = self.insert(root.left, current)
+                current.parrent = root
+                return current
+            elif current.data > root.data:
+                root.right = self.insert(root.right, current)
+                current.parrent = root
+                return current
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.parent = None
 
 def main():
-    gt.generate_file('data/bst.txt', 10)
+    # gt.generate_file('data/bst.txt', 10)
+    t = Tree()
+    t.insert(t.root, Node(2))
+    t.insert(t.root, Node(3))
+    t.insert(t.root, Node(1))
 
 if __name__ == '__main__':
     main()
